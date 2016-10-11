a5c8331fe5625dee1aad0bd8917bdad2a11a4739
Alex Carbajo <alexcarbajo@gmail.com>
2016-10-11 14:39:20 +0200

    Added cssnano

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 'use strict';
 
 /*
- * Express Dependencies
- */
+* Express Dependencies
+*/
 var express = require('express'),
-    fs = require('fs'),
-    path = require('path'),
-    app = express();
+fs = require('fs'),
+path = require('path'),
+app = express();
 
 var port = 3000;
 
 process.title = 'design-projects';
 
 /*
- * Use Handlebars for templating
- */
+* Use Handlebars for templating
+*/
 var hbs = require('express3-handlebars').create({
-    extname: '.hbs',
-    defaultLayout: 'default',
-    layoutsDir: 'views/templates/',
-    helpers: require('./views/helpers'),
-    partialsDir: 'views/partials/'
-  });
+  extname: '.hbs',
+  defaultLayout: 'default',
+  layoutsDir: 'views/templates/',
+  helpers: require('./views/helpers'),
+  partialsDir: 'views/partials/'
+});
 
 
 // For gzip compression
@@ -41,37 +41,37 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 /*
- * Routes
- */
+* Routes
+*/
 
- app.use(app.router);
+app.use(app.router);
 
 
 var controllerDir = path.join(__dirname, '/views/pages/');
 fs.readdir(controllerDir, function (err, files) {
-    if (err) { throw err; }
-    files
-        .filter(function (file) { return file.substr(-4) === '.hbs'; })
-        .forEach(function (file) {
-            try {
-                file = file.substring(0, file.indexOf('.'));
-                console.log("Listen: ", file);
-                app.all('/'+ file, function (request, response, next) {
-                    var data = {};
-                    try{
-                        data = require(path.join(__dirname, '/views/data/', file +".json"));
-                    }catch (error) { }
-                    response.render(file, data);
-                });
-            }catch (e) {
-                console.log('Error al cargar el controller: ' + file);
-                console.error(e);
-            }
-    });
+  if (err) { throw err; }
+  files
+  .filter(function (file) { return file.substr(-4) === '.hbs'; })
+  .forEach(function (file) {
+    try {
+      file = file.substring(0, file.indexOf('.'));
+      console.log("Listen: ", file);
+      app.all('/'+ file, function (request, response, next) {
+        var data = {};
+        try{
+          data = require(path.join(__dirname, '/views/data/', file +".json"));
+        }catch (error) { }
+        response.render(file, data);
+      });
+    }catch (e) {
+      console.log('Error al cargar el controller: ' + file);
+      console.error(e);
+    }
+  });
 });
 
 /*
- * Start it up
- */
+* Start it up
+*/
 app.listen(process.env.PORT || port);
 console.log('Express started on port ' + port);
